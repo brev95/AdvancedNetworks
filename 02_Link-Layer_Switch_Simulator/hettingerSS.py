054f9cc9fc6ed3690b9e018a41f549f7e831e478
Breven <breven95@comcast.net>
2017-03-02 20:38:52 -0600

    Got Traffic Simulator working and started on Switch Simulator

--- 02_Link-Layer_Switch_Simulator/hettingerSS.py
+++ 02_Link-Layer_Switch_Simulator/hettingerSS.py
@@ -4,7 +4,35 @@
 # Assignment: #2 - Link-Layer Switch Simulator
 # File Name: hettingerSS.py
 #
-# Description: 
+# Description: The switch simulator simulates the actions described for
+# a switch in Ch. 5 of Kurose.
 ########################################################################
 
+class Packet:
+	interfaceNum = 0
+	srcMAC = ""
+	destMAC = ""
+	def init(self):
+		self.data = []
+
+from socket import *
+from sys import *
+from time import *
+import cPickle as pickle
+import numpy
+
+# Check for usage error
+if len(argv) != 4:
+	print "usage: python hettingerSS.py <Port Number> <Maximum Table Size> <Maximum Aging Time>"
+	quit()
+
+# Variables
+port = int(argv[1])
+maxTableSize = int(argv[2])
+maxAgingTime = int(argv[3])
+table = [maxTableSize][2]
+
+# Set up socket
+sock = socket(AF_INET, SOCK_DGRAM)
+sock.bind(("", 6000))
 
--- 02_Link-Layer_Switch_Simulator/hettingerTS.py
+++ 02_Link-Layer_Switch_Simulator/hettingerTS.py
@@ -4,7 +4,90 @@
 # Assignment: #2 - Link-Layer Switch Simulator
 # File Name: hettingerTS.py
 #
-# Description: 
+# Description: The traffic simulator auto-generates an unending source
+# of link-layer frames and send them at random times to the switch
+# simulator from a variety of link-layer nodes to other link-layer nodes
 ########################################################################
+
+class Packet:
+	interfaceNum = 0
+	srcMAC = ""
+	destMAC = ""
+	def init(self):
+		self.data = []
+
+from socket import *
+from sys import *
+from time import sleep
+import random
+import cPickle as pickle
+import numpy
+
+# Check for usage error
+if len(argv) != 5:
+	print "usage: python hettingerTS.py <IP Address> <Port Number> <Config Filename> <Max Random Interval>"
+	quit()
+
+# Variables
+ipAddr = argv[1]
+port = int(argv[2])
+configFileName = argv[3]
+maxRandInterval = int(argv[4])
+configTable = []
+
+numLines = sum(1 for line in open(configFileName))
+
+configFile = open(configFileName)
+
+for i in range(1, numLines):
+	configLine = configFile.readLine()
+	configTable.append(configLine.split(" "))
+
+
+
+# Set up socket
+sock = socket(AF_INET, SOCK_DGRAM)
+
+while(1):
+	timeInt = random.randint(0, maxRandInterval)
+	
+	randLine1 = random.randint(1, numLines) - 1
+	randLine2 = random.randint(1, numLines) - 1
+	
+	packet = Packet()
+	packet.interfaceNum = configTable[randLine1][0]
+	packet.srcMAC = configTable[randLine1][1]
+	packet.destMAC = configTable[randLine2][1]
+	
+	sendPacket - pickle.dumps(packet)
+	
+	sock.sendto(sendPacket, (ipAddr, port))
+	
+	sleep(timeInt)
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
